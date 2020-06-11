d58191dec2d105fbd207da4db780e3a28d5a4b48
lucasromero42 <63105849+lucasromero42@users.noreply.github.com>
2020-06-11 19:34:13 -0300

    playlist script

--- assets/script/app.js
+++ assets/script/app.js
@@ -144,12 +144,65 @@ const renderTrackList = (tracks, el) => {
 
 	// Add tracks to list
 	tracks.forEach((track, i) => {
+		let id = `${track.id}`;
 		let duration = moment.unix(track.duration).format('m:ss');
 		el.querySelector('ul').innerHTML += `
 			<li class="list-group-item list-group-item-dark list-group-item-small">
 			<p class="mb-0"><span class="mr-2">${i + 1}.</span> ${track.title}</p>
-			<p class="mb-0 text-muted">${duration}<i class="far fa-play-circle ml-4 text-white"></i></p>
-			</li>`;
+			<p class="mb-0 text-muted">${duration}<button class="far fa-play-circle ml-4 text-white" id="`+ id +`&" onclick="iframe(this.id)"></button></p>
+			</li><button class="agregar" id="`+ id +`" onclick="add(this.id)"></button>`;
+		window.iframe = function(clicked_id) {
+		let player = document.querySelector('iframe');
+		player.src = `https://www.deezer.com/plugins/player?format=classic&autoplay=false&playlist=true&layout=dark&size=medium&type=tracks&id=` + clicked_id + `app_id=1`;
+		
+		};
+		window.add = function(clicked_id) {
+        //playlist
+        let recuperoStorage = localStorage.getItem('playlist');
+
+        //Si todavía no tengo tracks en mi playlist
+        if(recuperoStorage == null){
+            //Creo una lista vacía
+            playlist = [];
+        } else {
+            //Recupero el array de localStorage
+            playlist = JSON.parse(recuperoStorage);
+        }
+
+        //Me fijo que no esté en la lista y cambio el texto del botón
+        if(playlist.includes(clicked_id)){
+            document.getElementById(clicked_id).innerHTML = "Quitar de la playlist";
+        }
+
+        //Paso 2: agregar un track a la playlist.
+        let agregar = document.getElementById(clicked_id);
+
+        agregar.addEventListener('click', function(e){
+            //Detener el comportamiento default de <a></a>
+            e.preventDefault();
+
+            if(playlist.includes(clicked_id)){
+                //Si el track está tenemos que quitarlo.
+                let indiceEnElArray = playlist.indexOf(clicked_id);
+                playlist.splice(indiceEnElArray, 1);
+                document.getElementById(clicked_id ).innerHTML = "Agregar a playlist";
+                console.log(playlist);
+
+            } else {
+                //Agrego el id del track a la lista
+                playlist.push(clicked_id);
+                document.getElementById(clicked_id).innerHTML = "Quitar de la playlist"
+            }
+            //
+
+
+            //Paso 3 guardar lista en localStorage
+            let playlistParaStorage = JSON.stringify(playlist);
+            localStorage.setItem('playlist', playlistParaStorage);
+            console.log(localStorage);
+            });
+
+        };
 	});
 };
 

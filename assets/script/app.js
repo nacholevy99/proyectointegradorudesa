2aa429c6c08d0addf63f3013fbe9e76ae56d29d7
lucasromero42 <63105849+lucasromero42@users.noreply.github.com>
2020-06-11 20:47:08 -0300

    g

--- assets/script/app.js
+++ assets/script/app.js
@@ -1,4 +1,3 @@
-
 const searchForm = document.querySelector('#search-form');
 const searchResultEL = document.querySelector('#search-result-wrapper');
 const albumsEl = document.querySelector('#search-albums');
@@ -136,60 +135,60 @@ const renderTrackResult = track => {
 					<a href="#" data-artist="${track.artist.id}">${track.artist.name}</a>,
 					<a href="#" data-album="${track.album.id}">${track.album.title}</a></p>
 					<p class="mb-0 text-muted">`+ duration +`<button class="far fa-play-circle ml-4 text-white" id="`+ id +`&" onclick="iframe(this.id)"></button></p>
-					</li><button class="agregar" id="`+ id +`" onclick="add(this.id)"></button>`;
-};
-window.iframe = function(clicked_id) {
-	let player = document.querySelector('iframe');
-	player.src = `https://www.deezer.com/plugins/player?format=classic&autoplay=false&playlist=true&layout=dark&size=medium&type=tracks&id=` + clicked_id + `app_id=1`;
-	};
-	window.add = function(clicked_id) {
-	//playlist
-	let recuperoStorage = localStorage.getItem('playlist');
+			</li><button class="agregar" id="`+ id +`" onclick="add(this.id)"></button>`;
+		window.iframe = function(clicked_id) {
+		let player = document.querySelector('iframe');
+		player.src = `https://www.deezer.com/plugins/player?format=classic&autoplay=false&playlist=true&layout=dark&size=medium&type=tracks&id=` + clicked_id + `app_id=1`;
+		};
+		window.add = function(clicked_id) {
+        //playlist
+        let recuperoStorage = localStorage.getItem('playlist');
 
-	//Si todavía no tengo tracks en mi playlist
-	if(recuperoStorage == null){
-		//Creo una lista vacía
-		playlist = [];
-	} else {
-		//Recupero el array de localStorage
-		playlist = JSON.parse(recuperoStorage);
-	}
+        //Si todavía no tengo tracks en mi playlist
+        if(recuperoStorage == null){
+            //Creo una lista vacía
+            playlist = [];
+        } else {
+            //Recupero el array de localStorage
+            playlist = JSON.parse(recuperoStorage);
+        }
 
-	//Me fijo que no esté en la lista y cambio el texto del botón
-	if(playlist.includes(clicked_id)){
-		document.getElementById(clicked_id).innerHTML = "Quitar de la playlist";
-	}
+        //Me fijo que no esté en la lista y cambio el texto del botón
+        if(playlist.includes(clicked_id)){
+            document.getElementById(clicked_id).innerHTML = "Quitar de la playlist";
+        }
 
-	//Paso 2: agregar un track a la playlist.
-	let agregar = document.getElementById(clicked_id);
+        //Paso 2: agregar un track a la playlist.
+        let agregar = document.getElementById(clicked_id);
 
-	agregar.addEventListener('click', function(e){
-		//Detener el comportamiento default de <a></a>
-		e.preventDefault();
+        agregar.addEventListener('click', function(e){
+            //Detener el comportamiento default de <a></a>
+            e.preventDefault();
 
-		if(playlist.includes(clicked_id)){
-			//Si el track está tenemos que quitarlo.
-			let indiceEnElArray = playlist.indexOf(clicked_id);
-			playlist.splice(indiceEnElArray, 1);
-			document.getElementById(clicked_id ).innerHTML = "Agregar a playlist";
-			console.log(playlist);
+            if(playlist.includes(clicked_id)){
+                //Si el track está tenemos que quitarlo.
+                let indiceEnElArray = playlist.indexOf(clicked_id);
+                playlist.splice(indiceEnElArray, 1);
+                document.getElementById(clicked_id ).innerHTML = "Agregar a playlist";
+                console.log(playlist);
 
-		} else {
-			//Agrego el id del track a la lista
-			playlist.push(clicked_id);
-			document.getElementById(clicked_id).innerHTML = "Quitar de la playlist"
-		}
-		//
+            } else {
+                //Agrego el id del track a la lista
+                playlist.push(clicked_id);
+                document.getElementById(clicked_id).innerHTML = "Quitar de la playlist"
+            }
+            //
 
 
-		//Paso 3 guardar lista en localStorage
-		let playlistParaStorage = JSON.stringify(playlist);
-		localStorage.setItem('playlist', playlistParaStorage);
-		console.log(localStorage);
-		});
+            //Paso 3 guardar lista en localStorage
+            let playlistParaStorage = JSON.stringify(playlist);
+            localStorage.setItem('playlist', playlistParaStorage);
+            console.log(localStorage);
+            });
 
-	};
-	
+        };
+        }; 
+
 const renderTrackList = (tracks, el) => {
 
 	el.querySelector('ul').innerHTML = '';

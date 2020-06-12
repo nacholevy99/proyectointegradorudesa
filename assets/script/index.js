0e17a5e43127d3666fec7b1cf8ffa4f0f3b2d7b8
arigarfunkel1 <63112843+arigarfunkel1@users.noreply.github.com>
2020-06-12 14:08:46 -0300

    Create index.js

---
+++ assets/script/index.js
@@ -0,0 +1,10 @@
+function myFunction() {
+  let search = document.getElementById("search").value;
+  if (typeof(Storage) !== "undefined") {
+    // Store
+    localStorage.setItem("search", search);
+    window.location.href = "detail.html";
+  } else {
+    document.getElementById("result").innerHTML = "Sorry, your browser does not support Web Storage...";
+  }
+}

e73e8d1865990aa9f95a2a178b9d25511ba5f521
arigarfunkel1 <63112843+arigarfunkel1@users.noreply.github.com>
2020-06-16 14:36:00 -0300

    Update resultados.js

--- assets/script/resultados.js
+++ assets/script/resultados.js
@@ -12,7 +12,10 @@ let proxy = 'https://cors-anywhere.herokuapp.com/';
 let url =  `${proxy}https://api.deezer.com/search?q=${search}&limit=5`;
 let urlartist = `${proxy}https://api.deezer.com/search/artist?q=${search}&limit=5`;
 let urlalbum = `${proxy}https://api.deezer.com/search/album?q=${search}&limit=5`;
-
+window.iframe = function(clicked_id) {
+    let player = document.querySelector('iframe');
+    player.src = `https://www.deezer.com/plugins/player?format=classic&autoplay=false&playlist=true&layout=dark&size=medium&type=tracks&id=${clicked_id}app_id=1`;
+  };
 fetch(url)
     .then(function(response){
         return response.json();
@@ -22,19 +25,19 @@ fetch(url)
         console.log(datos);
         let lista = document.querySelector('.lista');
         let resultados = datos.data;
-        
-       //Tracks, linea 29 boton para escuchar la musica// 
+
+       //Tracks, linea 29 boton para escuchar la musica//
         resultados.forEach(function(resultado){
             lista.innerHTML += `<li>${resultado.title} By <a href="detail.html?artists=${resultado.artist.id}"> ${resultado.artist.name}</a>
-            <button id="${resultado.id}" onclick="iframe(this.id)"></button></li>` 
-            
+            <button id="${resultado.id}" onclick="iframe(this.id)"></button></li>`
+
         });
-        
-        
+
+
     })
     .catch(function(error){
         console.log(error);
-        
+
     });
 fetch(urlartist)
     .then(function(response){
@@ -45,16 +48,16 @@ fetch(urlartist)
         console.log(datos);
         let artistas = document.querySelector('.artistas');
         let resultados = datos.data;
-        
+
         resultados.forEach(function(resultado){
             artistas.innerHTML += `<li><a href="detail.html?artists=${resultado.id}"> ${resultado.name}</a></li>`
         });
-        
-        
+
+
     })
     .catch(function(error){
         console.log(error);
-        
+
     });
 fetch(urlalbum)
     .then(function(response){
@@ -65,18 +68,12 @@ fetch(urlalbum)
         console.log(datos);
         let albumes = document.querySelector('.albumes');
         let resultados = datos.data;
-        
+
         resultados.forEach(function(resultado){
             albumes.innerHTML += `<li><a href="detail.html?albums=${resultado.id}">${resultado.title}</a></li>`
         });
     })
     .catch(function(error){
         console.log(error);
-        
+
     });
-    window.iframe = function(clicked_id) {
-        let player = document.querySelector('iframe');
-        player.src = `https://www.deezer.com/plugins/player?format=classic&autoplay=false&playlist=true&layout=dark&size=medium&type=tracks&id=${clicked_id}app_id=1`;
-      };
-      
-     

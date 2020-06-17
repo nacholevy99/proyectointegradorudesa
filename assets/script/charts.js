3062501f3429ad6e8d7f496088f9a497abffa6be
nacholevy99 <juanignaciolevy@gmail.com>
2020-06-17 18:02:07 -0300

    Termino top charts index

--- assets/script/charts.js
+++ assets/script/charts.js
@@ -2,6 +2,8 @@
 
 let proxy = 'https://cors-anywhere.herokuapp.com/';
 let url =  proxy + "https://api.deezer.com/chart/0/tracks";
+let urlalbum = proxy + "https://api.deezer.com/chart/0/albums"
+let urlartist = proxy + "https://api.deezer.com/chart/0/artists"
 
 
 fetch(url) 
@@ -27,3 +29,57 @@ fetch(url)
         .catch(function(error){
             console.log(error);
         })
+
+
+//ARTIST//
+ fetch(urlartist) 
+        .then(function(response){
+            return response.json();
+        })
+    
+        .then(function(datos){
+            console.log(datos);
+    
+             let artists = document.querySelector('.artists');
+             let resultados = datos.data;
+    
+            resultados.forEach(function(artist){
+                    artists.innerHTML += '<li>' + '<a href="artist.html?id=' 
+                    + artist.id + '">' + artist.name + '</a></li>'
+    
+                })
+    
+    
+            })
+    
+            .catch(function(error){
+                console.log(error);
+            })
+
+
+
+//ALBUM//
+
+fetch(urlalbum) 
+        .then(function(response){
+            return response.json();
+        })
+    
+        .then(function(datos){
+            console.log(datos);
+    
+             let albums = document.querySelector('.albums');
+             let resultados = datos.data;
+    
+            resultados.forEach(function(album){
+                    albums.innerHTML += '<li>' + '<a href="album.html?id=' 
+                    + album.id + '">' + album.title + '</a></li>'
+    
+                })
+    
+    
+            })
+    
+            .catch(function(error){
+                console.log(error);
+            })

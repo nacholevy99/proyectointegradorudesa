d9a958a1049f54a1fd1231b73796827fa91660df
lucasromero42 <63105849+lucasromero42@users.noreply.github.com>
2020-06-18 17:27:27 -0300

    Revert "boton de tracks"
    This reverts commit b9bb59079d63822044005910a83ce479bd5fea91.

--- assets/script/detail.js
+++ assets/script/detail.js
@@ -156,8 +156,7 @@ if(track) {
           let results = document.querySelector('.results');
           let resultados = datos;
           results.innerHTML += `<li style="color:white">Artist: ${resultados.artist.name} <img src="${resultados.artist.picture_big}"/> Track: ${resultados.title} Album: ${resultados.album.title} <img src="${resultados.album.cover_big}"/></li>`
-          `<li><button>${resultado.title}</button><button id="${resultado.id}" onclick="iframe(this.id)"></button></li>`
-        })
+      })
       .catch(function (error) {
           console.log(error);
 
@@ -191,7 +190,7 @@ if(artists) {
             let resultados = datos.data;
 
             resultados.forEach(function(resultado){
-                tracklist.innerHTML += ` <li>  <button>${resultado.title}</button><button id="${resultado.id}" onclick="iframe(this.id)"></button></li>`
+                tracklist.innerHTML += `<li>  <button>${resultado.title}</button><button id="${resultado.id}" onclick="iframe(this.id)"></button></li>`
             });
         })
         .catch(function(error){

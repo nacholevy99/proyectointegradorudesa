0d75718eb079ea8194efcc8b1f8f74cbffad6693
lucasromero42 <63105849+lucasromero42@users.noreply.github.com>
2020-06-18 15:05:59 -0300

    detail tracks

--- assets/script/detail.js
+++ assets/script/detail.js
@@ -20,6 +20,8 @@ let genre = `${proxy}https://api.deezer.com/genre/${genres}?limit=3`
 let genreartists = `${proxy}https://api.deezer.com/genre/${genres}/artists?limit=5`
 let genretracks = `${proxy}https://api.deezer.com/genre/${genres}/radios?limit=5`
 let genreradio = `${proxy}https://api.deezer.com/radio/${radio}/tracks`
+let proxy = 'https://cors-anywhere.herokuapp.com/';
+let urltracks = proxy + "https://api.deezer.com/track/" + idTrack
 window.iframe = function(clicked_id) {
   let player = document.querySelector('iframe');
   player.src = `https://www.deezer.com/plugins/player?format=classic&autoplay=false&playlist=true&layout=dark&size=medium&type=tracks&id=${clicked_id}app_id=1`;
@@ -143,6 +145,84 @@ if(radio) {
       });
 }
 //fin detalle generos
+//detailtracks
+fetch(urltracks)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(datos){
+        console.log(datos);
+
+        //Resolvemos qué hacemos con los datos
+        let titulo = document.querySelector('.titulo');
+        titulo.innerHTML += datos.title;
+
+        let interprete = document.querySelector('.interprete');
+        interprete.innerHTML += datos.artist.name
+
+        let album = document.querySelector('.album');
+        album.innerHTML += datos.album.title
+
+        //Aquí agregamos el player.
+        let player = document.querySelector('iframe');
+        player.src = 'https://www.deezer.com/plugins/player?format=classic&autoplay=false&playlist=true&width=700&height=350&color=007FEB&layout=dark&size=medium&type=tracks&id=' + idTrack + '&app_id=1'
+
+
+    })
+    .catch(function (error) {
+        console.log(error);
+
+    })
+
+
+//Pasos para agregar temas a una playlist
+//Paso 1: recuperar datos del storage
+
+let recuperoStorage = localStorage.getItem('playlist');
+
+//Si todavía no tengo tracks en mi playlist
+if(recuperoStorage == null){
+    //Creo una lista vacía
+    playlist = [];
+} else {
+    //Recupero el array de localStorage
+    playlist = JSON.parse(recuperoStorage);
+}
+
+//Me fijo que no esté en la lista y cambio el texto del botón
+if(playlist.includes(idTrack)){
+    document.querySelector('.agregar').innerHTML = "Quitar de la playlist";
+}
+
+//Paso 2: agregar un track a la playlist.
+let agregar = document.querySelector('.agregar');
+
+agregar.addEventListener('click', function(e){
+    //Detener el comportamiento default de <a></a>
+    e.preventDefault();
+
+    if(playlist.includes(idTrack)){
+        //Si el track está tenemos que quitarlo.
+        let indiceEnElArray = playlist.indexOf(idTrack);
+        playlist.splice(indiceEnElArray, 1);
+        document.querySelector('.agregar').innerHTML = "Agregar a playlist";
+        console.log(playlist);
+
+    } else {
+        //Agrego el id del track a la lista
+        playlist.push(idTrack);
+        document.querySelector('.agregar').innerHTML = "Quitar de la playlist"
+    }
+    //
+
+
+    //Paso 3 guardar lista en localStorage
+    let playlistParaStorage = JSON.stringify(playlist);
+    localStorage.setItem('playlist', playlistParaStorage);
+    console.log(localStorage);
+
+
+})
 
 //Detalle de artistas
 if(artists) {

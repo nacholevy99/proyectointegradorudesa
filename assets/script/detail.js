efe8d97d2648ef9296409af2e82cc543b8dc58f4
arigarfunkel1 <63112843+arigarfunkel1@users.noreply.github.com>
2020-06-18 22:17:18 -0300

    Update detail.js

--- assets/script/detail.js
+++ assets/script/detail.js
@@ -160,7 +160,7 @@ if(track) {
           ${resultados.artist.name} <img src="${resultados.artist.picture_big}"/>
           Track: ${resultados.title}
           Album: ${resultados.album.title} <img src="${resultados.album.cover_big}"/>
-          <button id="${resultados.id}" onclick="add(this.id)">Add</button></li>`
+          <button id="${resultados.id}" onclick="add(this.id)">Add</button> <button id="${resultados.id}" onclick="iframe(this.id)"> Play Now </button></li>`
 
 
         })

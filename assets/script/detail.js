b4634251d8c8a75025aca1b688014e1a025bf879
lucasromero42 <63105849+lucasromero42@users.noreply.github.com>
2020-06-18 21:18:20 -0300

    detail tracks

--- assets/script/detail.js
+++ assets/script/detail.js
@@ -155,8 +155,13 @@ if(track) {
           console.log(datos);
           let results = document.querySelector('.results');
           let resultados = datos;
-          results.innerHTML += `<li style="color:white">Artist: ${resultados.artist.name} <img src="${resultados.artist.picture_big}"/> Track: ${resultados.title} Album: ${resultados.album.title} <img src="${resultados.album.cover_big}"/></li>`
-      })
+          results.innerHTML += `<li style="color:white">Artist: 
+          ${resultados.artist.name} <img src="${resultados.artist.picture_big}"/> 
+          Track: ${resultados.title} 
+          Album: ${resultados.album.title} <img src="${resultados.album.cover_big}"/></li>`
+      
+      
+        })
       .catch(function (error) {
           console.log(error);
 
@@ -190,7 +195,7 @@ if(artists) {
             let resultados = datos.data;
 
             resultados.forEach(function(resultado){
-                tracklist.innerHTML += `<li>  <button>${resultado.title}</button><button id="${resultado.id}" onclick="iframe(this.id)"></button></li>`
+                tracklist.innerHTML +=  `<li> <a href="detail.html?track=${resultado.id}"><div class="grid-item">${resultado.title}</a><button id="${resultado.id}" onclick="iframe(this.id)"> Play Now </button></li></div>`
             });
         })
         .catch(function(error){
